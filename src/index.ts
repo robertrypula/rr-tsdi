8823544e4547cdc9b5e3d82a194394b416e856b6
Robert Rypuła <robert.rypula@gmail.com>
2018-02-04 23:41:58 +0100

    Configuration updates (still not stable)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export const version = '1.0.2';
-export * from './public-api';
+import staticImplements from './static-implements/static-implements';
+
+export const version = '1.0.3';
+export { Injector, InjectorInterface, InjectorStatic } from './injector/injector';
+export { staticImplements };
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,79 @@
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const WrapperPlugin = require('wrapper-webpack-plugin');
-const { readFileSync } = require("fs");
+const { readFileSync } = require('fs');
 const LICENCE = readFileSync(path.resolve(__dirname) + '/LICENCE');
+let config;
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
+// TODO read version from code and name from package.json
+
+function getConfig() {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts','.js']
+    },
+    output: {
+      filename: '[name].js',
+      library: 'RrTsdi',
+      libraryTarget: 'umd',
+      path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+      new WrapperPlugin({
+        header: '/*\n' + LICENCE + '*/\n\n'
+      })
     ]
-  },
-  devServer: {
+  };
+}
+
+function fillDev(config) {
+  config.entry = {
+    'rr-tsdi-v1.0.3': './src/index.ts'
+  };
+
+  config.devtool = 'inline-source-map';
+
+  config.devServer = {
     contentBase: path.resolve(__dirname),
     publicPath: '/dist/',
     compress: true,
-    port: 9000,
+    port: 8000,
     hot: false,
     openPage: 'example/index.html',
-    overlay: true
-  },
-  resolve: {
-    extensions: [
-      '.ts',
-      '.js'
-    ]
-  },
-  output: {
-    filename: 'rr-tsdi-v1.0.2.js',
-    library: 'RrTsdi',
-    libraryTarget: 'umd',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    // new UglifyJsPlugin({
-    //   sourceMap: true
-    // }),
-    new WrapperPlugin({
-      header: '/*\n' + LICENCE + '*/\n\n'
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  };
+}
+
+function fillProd(config) {
+  config.entry = {
+    'rr-tsdi-v1.0.3': './src/index.ts',
+    'rr-tsdi-v1.0.3.min': './src/index.ts',
+  };
+
+  config.devtool = 'source-map';
+
+  config.plugins.unshift(
+    new UglifyJsPlugin({
+      include: /\.min\.js$/,
+      sourceMap: true
     })
-  ]
-};
+  );
+}
+
+config = getConfig();
+fillDev(config);
+// fillProd(config);
+
+module.exports = config;
